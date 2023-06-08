e70dca90b318b1350a37d9604e7ca0e986aab987
JGaviria0 <j.gaviria@utp.edu.co>
2023-06-08 13:03:02 -0500

    Database and signup

--- database/db.sql
+++ database/db.sql
@@ -3,42 +3,39 @@ CREATE DATABASE dabase_links;
 USE dabase_links; 
 
 CREATE TABLE users(
-    id INT(11) NOT NULL,
+    id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(16) NOT NULL, 
     password VARCHAR(60) NOT NULL,
-    fullname VARCHAR(100) NOT NULL
+    document_type VARCHAR(100) NOT NULL, 
+    first_name VARCHAR(100) NOT NULL, 
+    last_name VARCHAR(100) NOT NULL, 
+    email VARCHAR(100) NOT NULL, 
+    identity_document VARCHAR(100) NOT NULL, 
+    cellphone_number VARCHAR(100) NOT NULL, 
+    user_type VARCHAR(100) NOT NULL,
+    birth_date timestamp NOT NULL
 );
 
 ALTER TABLE users 
-    ADD PRIMARY KEY(id); 
+    ADD birth_date timestamp NOT NULL;
 
 ALTER TABLE users
     MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2; 
 
 DESCRIBE users; 
 
-
 CREATE TABLE links (
-    id INT(11) NOT NULL,
+    id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(150) NOT NULL,
     url VARCHAR(250) NOT NULL,
     description TEXT, 
     user_id INT(11),
     created_at timestamp NOT NULL DEFAULT current_timestamp,
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id),
+    estado VARCHAR(50) NOT NULL DEFAULT 'Disponible'
 );
 
-ALTER TABLE links
-    ADD PRIMARY KEY (id); 
-
-ALTER TABLE links 
-    MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2; 
-
 DESCRIBE links; 
-
-ALTER TABLE links
-  ADD estado VARCHAR(50) NOT NULL DEFAULT 'Disponible';
-
 
 CREATE TABLE rentados (
     id INT(11) NOT NULL,
