e9f21b6cd2782b6f0d9b66a9c08155ee6db58597
Karen Grajales <karen.grajales@utp.edu.co>
2023-06-10 22:43:59 -0500

    (owner): update vehicles was created

--- src/routes/links.js
+++ src/routes/links.js
@@ -112,13 +112,13 @@ router.get('/devuelto/:id', isSuperRoot, async (req, res) => {
 })
 
 
-router.get('/edit/:id', isSuperRoot, async (req, res) => {
+router.get('/edit/:id', isLoggedIn, async (req, res) => {
     const { id } = req.params 
     const links = await pool.query('SELECT * FROM links WHERE id = ?', [id])
     res.render('links/edit', { link: links[0] })
 })
 
-router.post('/edit/:id', isSuperRoot, async(req, res) => {
+router.post('/edit/:id', isLoggedIn, async(req, res) => {
     const { id } = req.params
     const { title, description, url } = req.body
     const newLink = { 
